ac3f1557a42e4b9bf96f9b8c7bad46b7f00ed28c
Anindo Roy <anindoroy112@gmail.com>
2025-01-21 10:41:16 +0600

    route update

--- src/app/manage/components/sidebar-admin/SidebarAdminWeb.tsx
+++ src/app/manage/components/sidebar-admin/SidebarAdminWeb.tsx
@@ -94,7 +94,7 @@ const SidebarAdminWeb: React.FC = () => {
         </li>
         <li className="mb-1 min-w-60">
           <Link
-            to="/manage/institute"
+            to="/manage/teachers"
             className="group/link hover:text-white w-full flex justify-center items-center gap-2 py-[7px] px-2.5 bg-white hover:bg-secondary text-primary-700 hover:text-white-100 rounded-md transition relative z-20"
           >
             <div className="flex flex-row items-center w-full gap-4">
