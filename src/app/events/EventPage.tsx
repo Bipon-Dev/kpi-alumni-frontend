d1bfb5ef56858cb9761f8c7827ed6931e804a48c
Anindo Roy <anindoroy112@gmail.com>
2025-04-02 12:04:50 +0600

    update

--- src/app/events/EventPage.tsx
+++ src/app/events/EventPage.tsx
@@ -1,5 +1,23 @@
-import AdmEventProvider from "../manage/events/components/context/AdmEventProvider";
-import EventPageHeaderSection from "./components/EventPageHeaderSection";
+// import AdmEventProvider from "../manage/events/components/context/AdmEventProvider";
+// import EventPageHeaderSection from "./components/EventPageHeaderSection";
+import eventBg from "@/assets/images/kpi-event-bg.webp";
+
+const EventPageHeader = () => {
+  return (
+    <div className=" w-64 border h-100 p-4 rounded-lg shadow-lg">
+      <div className=" text-center font-medium text-xl pb-4">hello</div>
+      <div>
+        <img src={eventBg} alt="eventBg" className=" rounded-md" />
+      </div>
+      <div className=" text-ellipsis overflow-hidden line-clamp-4 text-sm pt-4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+        reiciendis sint cumque, cum voluptas velit ipsum? Dignissimos,
+        necessitatibus quisquam modi autem qui nesciunt ipsam reiciendis,
+        recusandae sunt, delectus labore sint.
+      </div>
+    </div>
+  );
+};
 
 const EventPage = () => {
   return (
@@ -11,6 +29,14 @@ const EventPage = () => {
         <h1 className="border-b-4 pb-4 border-primary">
           Khulna Polytechnic Institute Alumni Events
         </h1>
+      </div>
+
+      <div className="flex justify-center h-full gap-8">
+        {Array(4)
+          .fill(null)
+          .map((_, index) => (
+            <EventPageHeader key={index} />
+          ))}
       </div>
     </div>
   );
