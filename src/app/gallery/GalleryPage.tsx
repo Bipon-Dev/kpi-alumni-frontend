4a5ae0a813db65fcb57b715e22efe97c17591f44
Anindo Roy <anindoroy112@gmail.com>
2025-04-09 10:04:58 +0600

    Refactor GalleryPage layout and update image order in galleryImg array

--- src/app/gallery/GalleryPage.tsx
+++ src/app/gallery/GalleryPage.tsx
@@ -2,19 +2,16 @@ import { galleryImg } from "./image/image";
 
 const GalleryPage = () => {
   return (
-    <div className="home-Page pt-[70px] scrollbar-hide bg-gray-100">
+    <div className="home-Page container mx-auto pt-[70px] scrollbar-hide ">
       <div className="text-center text-4xl  font-bold my-10 text-primary flex justify-center ">
         <h1 className="border-b-4 pb-4 border-primary">
           Khulna Polytechnic Institute Alumni Gallery
         </h1>
       </div>
-      <div className=" flex flex-wrap gap-2  justify-center my-16">
+      <div className=" flex flex-wrap gap-4  justify-center my-16">
         {Array.isArray(galleryImg) &&
           galleryImg.map((img: any, index: number) => (
-            <div
-              key={index}
-              className="gallery-img bg-white p-2 h-[400px] w-auto "
-            >
+            <div key={index} className="gallery-img  h-[400px] w-auto ">
               <img src={img.src} alt={img.alt} className=" h-full" />
             </div>
           ))}
--- src/app/gallery/image/image.ts
+++ src/app/gallery/image/image.ts
@@ -11,15 +11,15 @@ import imgEleven from "./12.jpg";
 import imgTwelve from "./13.jpg";
 
 export const galleryImg = [
-  { id: 2, src: imgTwo },
-  { id: 3, src: imgThree },
-  { id: 4, src: imgFore },
   { id: 5, src: imgFive },
-  { id: 6, src: imgSix },
+  { id: 2, src: imgTwo },
   { id: 7, src: imgSeven },
+  { id: 3, src: imgThree },
   { id: 8, src: imgEight },
-  { id: 9, src: imgNine },
   { id: 10, src: imgTen },
-  { id: 11, src: imgEleven },
+  { id: 9, src: imgNine },
   { id: 12, src: imgTwelve },
+  { id: 4, src: imgFore },
+  { id: 6, src: imgSix },
+  { id: 11, src: imgEleven },
 ];
