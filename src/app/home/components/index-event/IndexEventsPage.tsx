b96e1e1fdf9f0bdea594b77619389e6030c4cd74
Anindo Roy <anindoroy112@gmail.com>
2025-04-14 13:53:30 +0600

    Fetch and display event data in IndexEventsPage; update layout for event listing

--- src/app/home/components/index-event/IndexEventsPage.tsx
+++ src/app/home/components/index-event/IndexEventsPage.tsx
@@ -1,36 +1,48 @@
-import { Button } from "@/lib/ui/button";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import eventLogo from "@/assets/logo/logo.png";
-const IndexEventsListComp: FC = () => {
+import axios from "axios";
+
+const IndexEventsListComp: FC<{ data: any }> = ({ data }) => {
   return (
     <div className="flex  gap-2 my-5 text-wrap">
       <div>
         <img src={eventLogo} alt="" className="!size-16" />
       </div>
       <div className="flex flex-col gap-1.5 w-[calc(100%-64px)]">
-        <span className=" text-sm text-primary">
-          Conservatory Exhibit: The garden of india a country and culture
-          revealed
-        </span>
-        <span className=" text-primary-200 text-xs">
-          Matthaei Botanical Gardens
+        <span className=" text-sm text-primary">{data.title}</span>
+        <span className=" text-primary-200 text-xs line-clamp-2">
+          {data.description}
         </span>
       </div>
     </div>
   );
 };
 const IndexEventsHead: FC = () => {
+  const [eventData, setEventData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5050/api/event");
+        setEventData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching event data:", error);
+      }
+    };
+
+    fetchEventData();
+  }, []);
   return (
     <div className=" flex flex-col justify-between m-5">
       <span className="text-2xl font-normal underline">Events</span>
       <div>
-        {[1, 2, 3, 4].map((item) => (
-          <IndexEventsListComp key={item} />
+        {eventData.slice(0, 3).map((event) => (
+          <IndexEventsListComp key={event.id} data={event} />
         ))}
       </div>
-      <Button variant="link" className="flex justify-start max-w-24 ">
+      <a href="/events" className="flex justify-start max-w-24 border-b ">
         All Events
-      </Button>
+      </a>
     </div>
   );
 };
